d609c48c316b14f5c2cf9ecc6905487a0a015f76
sushanthtambe <sushanth.tambe@gmail.com>
2023-12-26 18:29:31 +0530

    Done with  routing within Navbar Component

--- frontend/src/Components/Navbar/Navbar.tsx
+++ frontend/src/Components/Navbar/Navbar.tsx
@@ -5,7 +5,8 @@ import { NavLink, useLocation } from 'react-router-dom';
 import IconButton from '@mui/material/IconButton';
 import EastIcon from '@mui/icons-material/East';
 import Typography from '@mui/material/Typography';
-// import IecLogo from  ; 
+import IEClogo from '../../Images/Logo.svg';
+
 
 const NavBox = styled(Box)({
     margin:'20px',
@@ -16,58 +17,86 @@ const NavBox = styled(Box)({
     flexDirection:'row',
     padding:'10px',
     
+    
 
 })
 const Logo =styled(Box)({
-width:'200px',
+    width:'200px',
+    display:'flex',
+    left:'100px',
+    justifyContent:'start',
+    margin:'10px 400px 0px 50px'
+    
 })
 
 const Links =styled(Box)({
-    width:'300px',
+    width:'700px',
     display:'flex',
     flexDirection:'row',
     color:'white',
-    fontFamily:'inter'
+    fontFamily:''
 })
 const Resigter= styled(IconButton)({
     backgroundColor:'white',
     borderRadius:'10px',
-    margin:'10px',
+    margin:'10px 0px 10px 50px',
     right:'10px',
+    width:'220px',
+    display:'flex',
+    justifyContent:'start',
+    padding:'0px 0px 0px 20px ',
+    color:'black',
+    fontFamily:'sans-serif',
+    fontSize:'22px',
+
+})
+const LinkText =styled(Typography)({
+    fontFamily:'Sans-serif',
+    color:'white',
+    fontSize:'20px',
+    padding:'20px 40px'
 })
 
+const Arrow =styled(EastIcon)({
+    margin:'20px'
+})
 
 
 const Navbar = () => {
   return (
     <NavBox>
         <Logo>
-            <img src=""/>
+        <img src={IEClogo} height="50px"></img>
+            
             
         </Logo>
         
         <Links>
-        <Typography>Home</Typography>
-        <Typography>Events</Typography>
-        <Typography>Our team</Typography>
-        <Typography>About us </Typography>
+       
         
-        {/* <NavLink 
+        <NavLink 
             style={{textDecoration:'none'}}
             to='/home'         
-        >Home</NavLink>
+        >
+            <LinkText>Home</LinkText></NavLink>
         <NavLink 
             style={{textDecoration:'none'}}
             to='/events'           
-        >Home</NavLink>
+        >
+            <LinkText>Events</LinkText>
+        </NavLink>
         <NavLink 
             style={{textDecoration:'none'}}
             to='/ourteam'           
-        >Home</NavLink>
+        >
+            <LinkText>Our Team</LinkText>
+        </NavLink>
         <NavLink 
             style={{textDecoration:'none'}}
             to='/aboutus'        
-        >Home</NavLink> */}
+        >
+            <LinkText>About us</LinkText>
+        </NavLink>
         
        
 
@@ -75,7 +104,7 @@ const Navbar = () => {
     <Resigter>
         
         Register Now 
-        <EastIcon/>
+        <Arrow/>
         
     </Resigter>
     </NavBox>
