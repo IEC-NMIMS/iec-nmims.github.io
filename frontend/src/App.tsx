1394f3726462f0e64ed4d5cd1c8edce3bb407010
Yash Korlam <korlayash@gmail.com>
2024-01-01 14:53:04 +0530

    Code Cleanup

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,42 +1,18 @@
 import React from "react";
 import Navbar from "./Components/Navbar/Navbar";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Home from "./Components/Pages/Home";
-import Aboutus from "./Components/Pages/Aboutus";
-import Ourteam from "./Components/Pages/Ourteam";
 import Events from "./Components/Pages/Events";
-import Whoarewe from "./Components/Pages/Whoarewe";
-import BackgroundBox from "./Components/BackgroundBox/BackgroundBox";
 import WhatDo from "./Components/Pages/WhatDo";
-import Whatdowelearn from "./Components/Pages/Whatdowelearn";
+import WhoAreWe from "./Components/Pages/WhoAreWe";
+import WhatDoWeLearn from "./Components/Pages/WhatDoWeLearn";
 
 function App() {
 	return (
 		<>
 			<Navbar></Navbar>
-			<Whoarewe />
+			<WhoAreWe />
 			<WhatDo />
-			<Whatdowelearn />
+			<WhatDoWeLearn />
 			<Events />
-
-			{/* <Routes>
-				<Route
-					path="/"
-					element={<Home />}
-				/>
-				<Route
-					path="/aboutus"
-					element={<Aboutus />}
-				/>
-				<Route
-					path="/ourteam"
-					element={<Ourteam />}
-				/>
-				<Route
-					path="/events"
-					element={<Events />}
-				/>
-			</Routes> */}
 		</>
 	);
 }
--- frontend/src/Components/Pages/Whoarewe.tsx
+++ frontend/src/Components/Pages/Whoarewe.tsx
@@ -1,77 +1,123 @@
-import React from 'react'
+import React from "react";
 import Box from "@mui/material/Box";
 import styled from "@emotion/styled";
-import "./css/Whoarewe.css";
 import IconButton from "@mui/material/IconButton";
 import EastIcon from "@mui/icons-material/East";
+import Typography from "@mui/material/Typography";
 
-const Whoarewebox = styled(Box)({
-	margin: "20px",
-    backgroundColor: "rgba(42,43,42,255)",
+const OuterBox = styled(Box)({
+	width: "100%",
+	display: "flex",
+	justifyContent: "center",
+	border: "none",
+});
+
+const WhoAreWeBox = styled(Box)({
+	backgroundColor: "rgba(42,43,42,0.3)",
 	borderRadius: "20px",
-	height: "500px",
-    display: "flex",
+	width: "65%",
+	height: "325px",
+	display: "flex",
 	flexDirection: "row",
 	padding: "30px",
 	alignItems: "center",
-    color: "white",
-    border: "solid"
+	color: "white",
 });
 
-const Knowmore = styled(IconButton)({
+const LeftPanel = styled(Box)({
+	width: "60%",
+});
+
+const LeftPanelTitle = styled(Typography)({
+	fontSize: "3rem",
+	fontWeight: "400px",
+	marginBottom: "20px",
+});
+
+const LeftPanelPara = styled(Typography)({
+	fontSize: "16px",
+	marginBottom: "20px",
+});
+
+const KnowMore = styled(IconButton)({
 	backgroundColor: "rgb(0,185,203)",
 	borderRadius: "10px",
 	right: "10px",
-    height: "50px",
-	width: "450px",
+	height: "40px",
+	width: "350px",
 	display: "flex",
 	justifyContent: "start",
 	padding: "0px 0px 0px 20px ",
 	color: "black",
 	fontFamily: "sans-serif",
-	fontSize: "22px",
+	fontSize: "16px",
+	fontWeight: "bold",
+	marginLeft: "8px",
+
+	"&:hover": {
+		backgroundColor: "rgb(0,185,203)",
+	},
 });
 
 const Arrow = styled(EastIcon)({
-	marginLeft: "200px",
-    marginTop: "5px"
+	marginLeft: "auto",
+	marginTop: "5px",
+	marginRight: "15px",
 });
 
-const Whoarewe = () =>{
-    return(
-        <Whoarewebox>
-            <div className="whoarewediv1">
-                <p className="whoarewetitle">
-                    Who are we?
-                </p>
-                <p className="whoarewepara">
-                The Innovation and Entrepreneurship cell (IEC)<br /> 
-                of mpstme is a student body, certified by the<br /> 
-                Ministry of Education. Government of India, and<br /> 
-                aims to inculcate and entrepreneurial and innovative<br /> 
-                environment in the institute. We want to keep up the<br /> 
-                spirit of entrepreneurship in the minds of the youth<br /> 
-                and create passion amongst the students who<br /> 
-                wish to pursue you entrepreneurship as a career.<br />
-                </p>
-                <br />
-                <Knowmore>
-                    <div>
-                        Click to know more 
-                    </div>
-                    <div>
-                    <Arrow />
-                    </div>
-                </Knowmore>
-            </div>
-            <div className="whoarewediv2">
-                <img className="whoareweimg"
-                    src="Images/White.svg"
-                />
-            </div>
-        </Whoarewebox>
-        
-    )
-}
+const RightPanel = styled(Box)({
+	width: "40%",
+	padding: "40px",
+	display: "flex",
+	justifyContent: "end",
+});
 
-export default Whoarewe
+const RightPanelImage = styled(Box)({
+	alignItems: "center",
+	justifyContent: "end",
+	height: "250px",
+	width: "100%",
+	display: "flex",
+	borderRadius: "20px",
+	padding: "20px",
+	backgroundColor: "gray",
+});
+
+const WhoAreWe = () => {
+	return (
+		<OuterBox>
+			<WhoAreWeBox>
+				<LeftPanel>
+					<LeftPanelTitle>Who are we?</LeftPanelTitle>
+					<LeftPanelPara>
+						The Innovation and Entrepreneurship cell (IEC)
+						<br />
+						of mpstme is a student body, certified by the
+						<br />
+						Ministry of Education. Government of India, and
+						<br />
+						aims to inculcate and entrepreneurial and innovative
+						<br />
+						environment in the institute. We want to keep up the
+						<br />
+						spirit of entrepreneurship in the minds of the youth
+						<br />
+						and create passion amongst the students who
+						<br />
+						wish to pursue you entrepreneurship as a career.
+						<br />
+					</LeftPanelPara>
+					<KnowMore>
+						<div>Click to know more</div>
+						<Arrow />
+					</KnowMore>
+				</LeftPanel>
+				<RightPanel>
+					<RightPanelImage />
+				</RightPanel>
+			</WhoAreWeBox>
+		</OuterBox>
+	);
+};
+
+export default WhoAreWe;
