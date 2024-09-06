1edbbf8467e0b433147d83057da00985c951662e
Parth Jhaveri <jhaveriparth10@gmaiil.com>
2024-09-06 23:28:59 +0530

    changed the font sizes and the phone numbers and also changed events component to non scroll for large screens and scroll for small screens

--- src/Components/Events/EventList.ts
+++ src/Components/Events/EventList.ts
@@ -1,64 +1,64 @@
 export const EventList = [
-	{
-		image: "./images/event-carousel/enigma-banner.png",
-		name: "Enigma",
-		desc: "IEC's introductory event meant for freshers essentially to get the essence of 'our vision and mission'. Various innovative games including Time Capsule, where people write a note to their future selves about their goals, heads up and dumb charades of brands/entrepreneurs sum it up. It is a delightful experience with a Bollywood twist where freshers get hands-on experience about entrepreneurship.",
-		modalData: {
-			title: "Enigma Unveiled: Have a look!",
-			data: "IEC's introductory event meant for freshers essentially to get the essence of 'our vision and mission'. Various innovative games including Time Capsule, where people write a note to their future selves about their goals, heads up and dumb charades of brands/entrepreneurs sum it up. It is a delightful experience with a Bollywood twist where freshers get hands-on experience about entrepreneurship.",
-			images: [
-				"./images/events/enigma/image-1.jpg",
-				"./images/events/enigma/image-2.JPG",
-				"./images/events/enigma/image-3.JPG",
-				"./images/events/enigma/image-4.JPG",
-				"./images/events/enigma/image-5.JPG",
-				"./images/events/enigma/image-6.JPG",
-			],
-		},
-	},
-	{
-		image: "./images/event-carousel/unplanned-banner.png",
-		name: "Unplanned 3.0",
-		desc: "Unplanned is a captivating event, in which participants have to use their prompt reasoning to create on-the-spot business plans. All participants will be presented with images based on which they have to assemble a business plan Participants will be kept on their toes as their challenges will change momentarily. his event aims to encourage improvisational skills in students while providing them with a platform to hone their planning skills.",
+  {
+    image: "./images/event-carousel/enigma-banner.png",
+    name: "Enigma",
+    desc: "IEC's introductory event for freshers showcases 'our vision and mission' through innovative games like Time Capsule, where participants write future goal notes, and brand/entrepreneur-themed heads up and dumb charades. This delightful experience with a Bollywood twist gives freshers hands-on entrepreneurship experience.",
+    modalData: {
+      title: "Enigma Unveiled: Have a look!",
+      data: "IEC's introductory event meant for freshers essentially to get the essence of 'our vision and mission'. Various innovative games including Time Capsule, where people write a note to their future selves about their goals, heads up and dumb charades of brands/entrepreneurs sum it up. It is a delightful experience with a Bollywood twist where freshers get hands-on experience about entrepreneurship.",
+      images: [
+        "./images/events/enigma/image-1.jpg",
+        "./images/events/enigma/image-2.JPG",
+        "./images/events/enigma/image-3.JPG",
+        "./images/events/enigma/image-4.JPG",
+        "./images/events/enigma/image-5.JPG",
+        "./images/events/enigma/image-6.JPG",
+      ],
+    },
+  },
+  {
+    image: "./images/event-carousel/unplanned-banner.png",
+    name: "Unplanned 3.0",
+    desc: "Unplanned is a captivating event where participants must quickly create business plans based on given images. The challenges will change momentarily, keeping everyone on their toes. This event aims to boost your improvisational skills and provide a platform for students to enhance their planning abilities.",
 
-		modalData: {
-			title: "Pitch-perfect Moments: Glimpses from Unplanned!",
-			data: "Unplanned is a captivating event, in which participants have to use their prompt reasoning to create on-the-spot business plans. All participants will be presented with images based on which they have to assemble a business plan Participants will be kept on their toes as their challenges will change momentarily. his event aims to encourage improvisational skills in students while providing them with a platform to hone their planning skills.",
-			images: [
-				"./images/events/unplanned/image-1.JPG",
-				"./images/events/unplanned/image-2.JPG",
-				"./images/events/unplanned/image-3.JPG",
-				"./images/events/unplanned/image-4.JPG",
-				"./images/events/unplanned/image-5.jpeg",
-				"./images/events/unplanned/image-6.jpg",
-			],
-		},
-	},
-	{
-		image: "./images/inceptio/inceptio-banner.png",
-		name: "Inceptio 7.0",
-		desc: "NMIMS MPSTME's Innovation and Entrepreneurship Carnival is an exceptional annual three-day event fostering entrepreneurship, business, and innovation. It gathers young individuals citywide to collaborate on projects, fostering discovery and teamwork.",
-		modalData: {
-			title: "Innovation Challenge",
-			data: "The NMIMS MPSTME's Innovation and Entrepreneurship Carnival is an exceptional annual three-day event fostering entrepreneurship, business, and innovation. It gathers young individuals citywide to collaborate on projects, fostering discovery and teamwork. The festival offers diverse experiences including workshops led by industry professionals to learn idea origination, validation, and skill application, with opportunities to win cash prizes, certificates, internships, and access to Angel Investors. Aimed at innovators and entrepreneurial spirits, it's a chance to realize potential and engage in a lifetime experience!",
-			images: [],
-		},
-	},
-	{
-		image: "./images/event-carousel/concept-show-banner.png",
-		name: "Concept Show",
-		desc: "Concept Show, an event held by MPSTME's Innovation and entrepreneurship cell is the go to place if you want to see an exemplary display of projects, embedded with core educational values such as critical thinking, design thinking, showcasing their own unique thoughts and innovation.",
-		modalData: {
-			title: "Ideas Illuminated: Snapshots of the Concept Show",
-			data: "Concept Show, an event held by MPSTME's Innovation and entrepreneurship cell is the go to place if you want to see an exemplary display of projects, embedded with core educational values such as critical thinking, design thinking, showcasing their own unique thoughts and innovation. With the main aim of giving the students one final taste of the industrial world before they enter it by giving opportunities to them to meet some of the renowned people from the industry.",
-			images: [
-				"./images/events/concept-show/image-1.JPG",
-				"./images/events/concept-show/image-2.JPG",
-				"./images/events/concept-show/image-3.JPG",
-				"./images/events/concept-show/image-4.JPG",
-				"./images/events/concept-show/image-5.JPG",
-				"./images/events/concept-show/image-6.JPG",
-			],
-		},
-	},
+    modalData: {
+      title: "Pitch-perfect Moments: Glimpses from Unplanned!",
+      data: "Unplanned is a captivating event, in which participants have to use their prompt reasoning to create on-the-spot business plans. All participants will be presented with images based on which they have to assemble a business plan Participants will be kept on their toes as their challenges will change momentarily. his event aims to encourage improvisational skills in students while providing them with a platform to hone their planning skills.",
+      images: [
+        "./images/events/unplanned/image-1.JPG",
+        "./images/events/unplanned/image-2.JPG",
+        "./images/events/unplanned/image-3.JPG",
+        "./images/events/unplanned/image-4.JPG",
+        "./images/events/unplanned/image-5.jpeg",
+        "./images/events/unplanned/image-6.jpg",
+      ],
+    },
+  },
+  {
+    image: "./images/inceptio/inceptio-banner.png",
+    name: "Inceptio 7.0",
+    desc: "NMIMS MPSTME's Innovation and Entrepreneurship Carnival is an exceptional annual three-day event fostering entrepreneurship, business, and innovation. It gathers young individuals citywide to collaborate on projects, fostering discovery and teamwork.",
+    modalData: {
+      title: "Innovation Challenge",
+      data: "The NMIMS MPSTME's Innovation and Entrepreneurship Carnival is an exceptional annual three-day event fostering entrepreneurship, business, and innovation. It gathers young individuals citywide to collaborate on projects, fostering discovery and teamwork. The festival offers diverse experiences including workshops led by industry professionals to learn idea origination, validation, and skill application, with opportunities to win cash prizes, certificates, internships, and access to Angel Investors. Aimed at innovators and entrepreneurial spirits, it's a chance to realize potential and engage in a lifetime experience!",
+      images: [],
+    },
+  },
+  {
+    image: "./images/event-carousel/concept-show-banner.png",
+    name: "Concept Show",
+    desc: "Concept Show, an event held by MPSTME's Innovation and entrepreneurship cell is the go to place if you want to see an exemplary display of projects, embedded with core educational values such as critical thinking, design thinking, showcasing their own unique thoughts and innovation.",
+    modalData: {
+      title: "Ideas Illuminated: Snapshots of the Concept Show",
+      data: "Concept Show, an event held by MPSTME's Innovation and entrepreneurship cell is the go to place if you want to see an exemplary display of projects, embedded with core educational values such as critical thinking, design thinking, showcasing their own unique thoughts and innovation. With the main aim of giving the students one final taste of the industrial world before they enter it by giving opportunities to them to meet some of the renowned people from the industry.",
+      images: [
+        "./images/events/concept-show/image-1.JPG",
+        "./images/events/concept-show/image-2.JPG",
+        "./images/events/concept-show/image-3.JPG",
+        "./images/events/concept-show/image-4.JPG",
+        "./images/events/concept-show/image-5.JPG",
+        "./images/events/concept-show/image-6.JPG",
+      ],
+    },
+  },
 ];
--- src/Components/Events/Item.tsx
+++ src/Components/Events/Item.tsx
@@ -1,117 +1,127 @@
 import styled from "@emotion/styled";
-import { Box, Card, CardContent, Typography, useMediaQuery, useTheme } from "@mui/material";
+import {
+  Box,
+  Card,
+  CardContent,
+  Typography,
+  useMediaQuery,
+  useTheme,
+} from "@mui/material";
 
 import IconButton from "@mui/material/IconButton";
 import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
 import { useNavigate } from "react-router-dom";
 
 interface modals {
-	title: string;
-	data: string;
-	images: string[];
+  title: string;
+  data: string;
+  images: string[];
 }
 interface Props {
-	image: string;
-	name: string;
-	desc: string;
-	modalData: modals;
+  image: string;
+  name: string;
+  desc: string;
+  modalData: modals;
 }
 
 const Item = (props: Props) => {
-	const navigate = useNavigate();
-	const { image, name, desc, modalData } = props;
-	const theme = useTheme();
-	const isMobile = useMediaQuery(theme.breakpoints.down("lg"));
+  const navigate = useNavigate();
+  const { image, name, desc, modalData } = props;
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down("lg"));
 
-	const EventCard = styled(Card)({
-		width: isMobile && window.innerWidth < 1024 ? "95%" : "350px",
-		height: "600px",
-		padding: "5px",
-		backgroundColor: "rgba(42,43,42,.3)",
-		borderRadius: "20px",
-	});
+  const EventCard = styled(Card)({
+    width: isMobile && window.innerWidth < 1024 ? "95%" : "350px",
+    height: "600px",
+    padding: "5px",
+    backgroundColor: "rgba(42,43,42,.3)",
+    borderRadius: "20px",
 
-	const EventPic = styled(Box)({
-		display: "flex",
-		alignSelf: "center",
-		width: isMobile && window.innerWidth < 1024 ? "100%" : "320px",
-		height: "260px",
-		borderRadius: "10px",
-	});
+  });
 
-	const Image = styled("img")({
-		width: isMobile && window.innerWidth < 1024 ? "100%" : "320px",
-		height: isMobile && window.innerWidth < 1024 ? "" : "100%",
-		aspectRatio: isMobile && window.innerWidth < 1024 ? "1 / 1" : "",
-		borderRadius: "20px",
-	});
+  const EventPic = styled(Box)({
+    display: "flex",
+    alignSelf: "center",
+    width: isMobile && window.innerWidth < 1024 ? "100%" : "320px",
+    height: "260px",
+    borderRadius: "10px",
+  });
 
-	const EventName = styled(Typography)({
-		fontFamily: "ITCAvantGardeGothicStd",
-		fontSize: "1.5rem",
-		fontWeight: "bold",
-		color: "white",
-		margin: "10px 0",
-	});
+  const Image = styled("img")({
+    width: isMobile && window.innerWidth < 1024 ? "100%" : "320px",
+    height: isMobile && window.innerWidth < 1024 ? "" : "100%",
+    aspectRatio: isMobile && window.innerWidth < 1024 ? "1 / 1" : "",
+    borderRadius: "20px",
+  });
 
-	const EventDesc = styled(Typography)({
-		fontFamily: "ITCAvantGardeGothicStd",
-		fontSize: isMobile && window.innerWidth < 1024 ? "0.6rem" : "0.7rem",
-		marginBottom: "20px",
-		color: "rgba(255, 255, 255, 0.8)",
-	});
+  const EventName = styled(Typography)({
+    fontFamily: "ITCAvantGardeGothicStd",
+    fontSize: "1.5rem",
+    fontWeight: "bold",
+    color: "white",
+    margin: "10px 0",
+  });
 
-	const KnowMore = styled(IconButton)({
-		color: "white",
-		fontSize: "16px",
-		borderRadius: "0px",
-		margin: "10px 0 0 0",
-		position: "fixed",
-		bottom: "10px",
-	});
+  const EventDesc = styled(Typography)({
+    fontFamily: "ITCAvantGardeGothicStd",
+    fontSize: isMobile && window.innerWidth < 1024 ? "0.75rem" : "0.85rem",
+    marginBottom: "20px",
+    color: "rgba(255, 255, 255, 0.8)",
+  });
 
-	const KnowMoreText = styled(Typography)({
-		marginLeft: "-8px",
-		fontSize: "1.25rem",
-		fontFamily: "ITCAvantGardeGothicStd",
-	});
+  const KnowMore = styled(IconButton)({
+    color: "white",
+    fontSize: "16px",
+    borderRadius: "0px",
+    margin: "10px 0 0 0",
+    position: "fixed",
+    bottom: "10px",
+  });
 
-	const Arrow = styled(ArrowForwardIosIcon)({
-		color: "white",
-		paddingLeft: "20px",
-		fontSize: "1.25rem",
-	});
-	const handleKnowMore = (title: string, data: string, images: string[]) => {
-		if (title === "Innovation Challenge") {
-			navigate("/inceptio");
-		} else {
-			navigate("/events", {
-				state: {
-					title: title,
-					data: data,
-					images: images,
-				},
-			});
-		}
-	};
+  const KnowMoreText = styled(Typography)({
+    marginLeft: "-8px",
+    fontSize: "1.25rem",
+    fontFamily: "ITCAvantGardeGothicStd",
+  });
 
-	const Itemcard = (
-		<CardContent>
-			<EventPic>
-				<Image src={image} />
-			</EventPic>
-			<EventName>{name}</EventName>
-			<EventDesc>{desc}</EventDesc>
+  const Arrow = styled(ArrowForwardIosIcon)({
+    color: "white",
+    paddingLeft: "20px",
+    fontSize: "1.25rem",
+  });
+  const handleKnowMore = (title: string, data: string, images: string[]) => {
+    if (title === "Innovation Challenge") {
+      navigate("/inceptio");
+    } else {
+      navigate("/events", {
+        state: {
+          title: title,
+          data: data,
+          images: images,
+        },
+      });
+    }
+  };
 
-			<KnowMore
-				onClick={() => handleKnowMore(modalData.title, modalData.data, modalData.images)}
-			>
-				<KnowMoreText>Know More</KnowMoreText>
-				<Arrow />
-			</KnowMore>
-		</CardContent>
-	);
-	return <EventCard variant="outlined">{Itemcard}</EventCard>;
+  const Itemcard = (
+    <CardContent>
+      <EventPic>
+        <Image src={image} />
+      </EventPic>
+      <EventName>{name}</EventName>
+      <EventDesc>{desc}</EventDesc>
+
+      <KnowMore
+        onClick={() =>
+          handleKnowMore(modalData.title, modalData.data, modalData.images)
+        }
+      >
+        <KnowMoreText>Know More</KnowMoreText>
+        <Arrow />
+      </KnowMore>
+    </CardContent>
+  );
+  return <EventCard variant="outlined">{Itemcard}</EventCard>;
 };
 
 export default Item;
